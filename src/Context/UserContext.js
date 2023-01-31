94f6008f8aa828967075687734b8de0a23a0a3c8
Akilah Powell <atpjun@gmail.com>
2023-01-30 16:28:53 -0800

    added browswer router to indexjs and userprovider

---
+++ src/Context/UserContext.js
@@ -0,0 +1,12 @@
+const { createContext, useState } = require('react');
+
+
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  return <UserContext.Provider value= {{ user, setUser }}>{children} </UserContext.Provider>; 
+};
+
+export { UserProvider, UserContext}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { UserProvider } from './Context/UserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <UserProvider>
+        <TasksProivder>
+          <App />
+        </TasksProivder>
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
